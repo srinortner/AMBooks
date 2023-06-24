1c52843dbb99e13ac92a1fce62b568a26241c28f
Susi <e1526772@student.tuwien.ac.at>
2023-06-24 11:54:52 +0200

    basic setup books, contact, about page and navbar, footer,

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {BooksComponent} from "./pages/books/books.component";
+import {AboutComponent} from "./pages/about/about.component";
+import {ContactComponent} from "./pages/contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'books',component:BooksComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
